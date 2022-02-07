770eb21755247e7023b0e1ce6c40def56d696c3f
wcm2scty <nugget.dinner.asif@gmail.com>
2022-02-07 20:43:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const axios = require("axios").default
+const PORT = 8000
 const express = require("express")
 const cors = require("cors")
 require('dotenv').config()
 const app = express()
 
-app.listen(process.env.PORT, () => console.log("Server running on PORT: " + process.env.PORT))
+app.listen(PORT, () => console.log("Server running on PORT: " + PORT))
 
 app.use(cors())
 
